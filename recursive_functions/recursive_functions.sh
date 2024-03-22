fe16e0a473a3236fc9b1d5b8cbd9a8138165d6d4
Nathan J. Russell <nathan@blackpathway.com>
2024-03-22 19:10:49 -0400

    yes

--- recursive_functions/recursive_functions.sh
+++ recursive_functions/recursive_functions.sh
@@ -3,8 +3,8 @@ num2=12
 expected_result=5200300
 
 # Test num1 choose num2
-output=$(./bc $num1 $num2)
-expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
+output=$(./bc $num1 $num2);
+expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items.";
 if [ "$output" == "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
