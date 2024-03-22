8b0862da1fc127688729ea27700df9b0f41386a2
Nathan J. Russell <nathan@blackpathway.com>
2024-03-22 19:15:42 -0400

    yes

--- recursive_functions/recursive_functions.sh
+++ recursive_functions/recursive_functions.sh
@@ -20,7 +20,7 @@ expected_result=0
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
@@ -35,7 +35,7 @@ expected_result=0
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
@@ -49,7 +49,7 @@ expected_result=155117520
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
@@ -64,7 +64,7 @@ expected_result=1
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
@@ -78,7 +78,7 @@ expected_result=1
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
@@ -93,7 +93,7 @@ expected_result=13123110
 # Test num1 choose num2
 output=$(./bc $num1 $num2)
 expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" = "$expected_output" ] ; then
   echo "Pass: The program computed the correct output for $num1 choose $num2."
 else
   echo "Expected '$expected_output' but got: $output"
