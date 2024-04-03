da8f03e27c3338df059bf9c030b17b00ca9a99fb
Nathan Russell <nathan@blackpathway.com>
2024-04-03 18:15:36 -0400

    new

--- recursive_functions/recursive_functions.sh
+++ recursive_functions/recursive_functions.sh
@@ -1,105 +1,20 @@
+#!/bin/bash
 
-num1=25
-num2=12
-expected_result=5200300
+function test_output {
+  num1=$1
+  num2=$2
+  expected_result=$3
 
-# Test num1 choose num2
-output=$(./bc $num1 $num2);
-expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items.";
-if [ "$output" == "$expected_output" ] ; then
-  echo "Pass: The program computed the correct output for $num1 choose $num2."
-else
-  echo "Expected '$expected_output' but got: $output"
-  exit 1
-fi
+  output=$(./bc $num1 $num2)
+  expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
 
+  if [ "$output" == "$expected_output" ] ; then
+    echo "Pass: The program computed the correct output for $num1 choose $num2."
+  else
+    echo "Expected '$expected_output' but got: $output"
+    exit 1
+  fi
+}
 
-# num1=-4
-# num2=3
-# expected_result=0
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-
-# num1=15
-# num2=17
-# expected_result=0
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-# num1=30
-# num2=15
-# expected_result=155117520
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-
-# num1=30
-# num2=30
-# expected_result=1
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-# num1=30
-# num2=0
-# expected_result=1
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-
-# num1=28
-# num2=18
-# expected_result=13123110
-
-# # Test num1 choose num2
-# output=$(./bc $num1 $num2)
-# expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
-# if [ "$output" = "$expected_output" ] ; then
-#   echo "Pass: The program computed the correct output for $num1 choose $num2."
-# else
-#   echo "Expected '$expected_output' but got: $output"
-#   exit 1
-# fi
-
-
-exit 0;
+# Test cases
+test_output 25 12 5200300
