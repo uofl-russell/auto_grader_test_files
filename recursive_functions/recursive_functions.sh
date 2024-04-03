b450a917453c0450f2acb450abfcb831605d1728
Nathan Russell <nathan@blackpathway.com>
2024-04-03 18:57:21 -0400

    new stuff

--- recursive_functions/recursive_functions.sh
+++ recursive_functions/recursive_functions.sh
@@ -7,10 +7,7 @@ function test_output {
   output=$(./bc $num1 $num2)
   expected_output="There are $expected_result ways to choose $num2 items from a set of $num1 items."
 
-  if [ "$output" == "$expected_output" ] ; then
-    echo "Pass: The program computed the correct output for $num1 choose $num2."
-  else
-    echo "Expected '$expected_output' but got: $output"
+  if [ "$output" != "$expected_output" ] ; then
     exit 1
   fi
 }
